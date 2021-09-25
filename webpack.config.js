7bae6b0276618bb5d6f7403342111a5e45767545
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2021-09-25 14:11:09 +0800

    修改檔案

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,15 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/javascripts/api.js', // webpack 打包的起始檔案
   plugins: [
-    // new webpack.EnvironmentPlugin({
-    //   CLIENTID: 'v4cbdvjc8zf6pj6mrogkycdx0h51z0',
-    //   OAUTHTOKEN: 'Bearer 6enwr0jv7bz7r9jc1lzenekqckxq7z'
-    // }),
     new Dotenv()
   ],
   output: {
     path: path.resolve(__dirname, 'public'), // 打包完的檔案位置
     filename: 'api.min.js' // 打包後輸出的檔名
-  },
-  optimization: {
-    // We no not want to minimize our code.
-    // https://github.com/serverless-heaven/serverless-webpack/issues/345
-    minimize: false
   }
+  // optimization: {
+  //   // We no not want to minimize our code.
+  //   // https://github.com/serverless-heaven/serverless-webpack/issues/345
+  //   minimize: false
+  // }
 };
